b08d8672232abb8299abae068716249b0ad27484
umidzhondev <umidzhonfullstackdev@gmail.com>
2022-09-21 01:16:55 +0600

    Added UI , Functionality

---
+++ src/components/Card.js
@@ -0,0 +1,70 @@
+import React, { useRef, useState } from "react";
+import styled from "styled-components";
+import { cardFrontImage } from "../utils/constants";
+
+const Card = ({ item, replaceItems }) => {
+  const { src } = item;
+  const [isAddActive, setIsAddActive] = useState(false);
+
+  const cardHandler = (e) => {
+    setIsAddActive(true);
+  };
+
+  return (
+    <CubeItem
+      onClick={cardHandler}
+      className={`cube ${isAddActive ? "active-card" : ""}`}
+      style={{ order: replaceItems(item) }}
+    >
+      <div className="cube__item front">
+        <img src={cardFrontImage} alt="card pictures" />
+      </div>
+      <div className="cube__item back">
+        <img src={src} alt="card pictures" />
+      </div>
+    </CubeItem>
+  );
+};
+
+const CubeItem = styled.li`
+  margin: auto;
+  position: relative;
+  width: 200px;
+  height: 160px;
+  transform-style: preserve-3d;
+  transition: transform 2s ease;
+  list-style-type: none;
+  cursor: pointer;
+  outline: 3px solid #fff;
+
+  & > div {
+    position: absolute;
+    background-color: aliceblue;
+    width: inherit;
+    height: inherit;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &.active-card {
+    transform: rotateY(180deg);
+  }
+
+  .front {
+    transform: translate3d(0, 0, 1px);
+  }
+
+  .back {
+    transform: translate3d(0, 0, -1px) rotateY(180deg);
+  }
+`;
+
+export default Card;
---
+++ src/components/Cards.js
@@ -0,0 +1,52 @@
+import React, { useState,useEffect } from "react";
+import styled from "styled-components";
+import Card from "./Card";
+import { images as cardItems } from "../utils/constants";
+
+const Cards = () => {
+  const images = cardItems[0].aviation;
+  const imagesClone = [...images];
+  const [handlerCount,setHandlerCount] = useState(0);
+  const [data, setData] = useState(images.concat(imagesClone));
+  const [checkedItems,setCheckedItems] = useState([]);
+
+  const getRandomIndex = () => {
+    const lengthImagesIndex = [];
+    for (let i = 0; i < images.length; i++) {
+      lengthImagesIndex.push(i);
+    }
+    let randomIndex =
+      lengthImagesIndex[
+        Math.round(Math.random() * lengthImagesIndex.length )
+      ];
+    return randomIndex;
+  };
+
+  return (
+    <Wrapper>
+      <ul className="cards">
+        {data.map((item, index) => {
+          return <Card key={index}  replaceItems={getRandomIndex} item={item} />;
+        })}
+      </ul>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  min-height:100vh;
+}  .cards{
+    max-width: 1000px;
+    margin-inline:auto;
+    display:grid;
+    grid-template-columns: repeat(4,1fr);
+    grid-template-rows: repeat(4,1fr);
+    grid-gap:20px;
+    perspective:600px;
+  }
+`;
+
+export default Cards;
